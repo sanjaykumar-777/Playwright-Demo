0147be19be87bda1dcaf4b8fbb94f53fadb40b28
sanjay-kumar777 <sanjaykumar.murugan@growfin.ai>
2025-08-05 23:39:51 +0530

    How to verify text in playwright

---
+++ tests/verifyErrorMessage.spec.js
@@ -0,0 +1,20 @@
+const {test,expect} = require('@playwright/test');
+
+test("Verify Error Message", async function({page}) {
+//login
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', {name: 'Username'}).pressSequentially('Admin',{delay : 100}); // to press sequentially
+    await page.getByRole('textbox', { name: 'Password' }).pressSequentially('admin12', {delay:100});
+    await page.getByRole('button', { name: 'Login' }).click();
+    
+    //verify error message
+    const errMessage = await page.getByRole('alert').textContent();
+    expect(errMessage.includes("Invalid")).toBeTruthy();
+    expect(errMessage === "Invalid credentials").toBeTruthy();
+    
+    console.log("Error Message:", errMessage);
+    await page.close(); // close the page after test completion
+
+}
+)
+    
